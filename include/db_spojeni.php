579a449e19a750aaeb4111a143ccf7a32e6ce9a0
Jiří Wolf <jiri.wolf@jw.cz>
2021-06-10 13:13:44 +0200

    Připojení k databázi.

--- include/db_spojeni.php
+++ include/db_spojeni.php
@@ -1,22 +1,22 @@
 <?php
 
-//    if ($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+    if ($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
       $mysqli = new mysqli('localhost', 'root', '9#wB$7ppGgjC4g', 'iordinace');
       $mysqli->set_charset('utf8');
-//    }
+    }
      //Ebola
-//    elseif ($_SERVER['SERVER_ADDR'] == '95.168.206.196') {
-//      $mysqli = new mysqli('mysql2.ebola.cz', 'jwcz_u', 'admin2255', 'jwcz_iordinace');
-//      $mysqli->set_charset('utf8');
-//    }
+    elseif ($_SERVER['SERVER_ADDR'] == '95.168.206.196') {
+      $mysqli = new mysqli('mysql2.ebola.cz', 'jwcz_u', 'admin2255', 'jwcz_iordinace');
+      $mysqli->set_charset('utf8');
+    }
     // Ebola
-//    elseif ($_SERVER['SERVER_ADDR'] == '95.168.206.203') {
-//      $mysqli = new mysqli('mysql3.ebola.cz', 'iordinacecz_user', '8WK4tXwGP0Cl', 'iordinacecz_sql');
-//    }
+    elseif ($_SERVER['SERVER_ADDR'] == '95.168.206.203') {
+      $mysqli = new mysqli('mysql3.ebola.cz', 'iordinacecz_user', '8WK4tXwGP0Cl', 'iordinacecz_sql');
+    }
 
-//    if ($mysqli->connect_error) {
-//      die('Nepodařilo se připojit k MySQL serveru (' . $mysqli->connect_errno . ')' . $mysqli->connect_error);
-//    }
-    //echo 'Připojení proběhlo úspěšně ' . $mysqli->host_info . "\n";
+    if ($mysqli->connect_error) {
+      die('Nepodařilo se připojit k MySQL serveru (' . $mysqli->connect_errno . ')' . $mysqli->connect_error);
+    }
+    echo 'Připojení proběhlo úspěšně ' . $mysqli->host_info . "\n";
 
 ?>
