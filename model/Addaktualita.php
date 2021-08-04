4eade5a7180476185fccf7d82e75e7d429d7f305
Jiří Wolf <jiri.wolf@jw.cz>
2021-08-04 10:47:23 +0200

    Připojení k databázi.

--- model/Addaktualita.php
+++ model/Addaktualita.php
@@ -68,16 +68,20 @@ class Addaktualita
         //$mysqli = new mysqli('localhost', 'root', 'heslo', 'iordinace');
         //$mysqli->set_charset('utf8');
 
-        if ($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+        if ($_SERVER['SERVER_NAME'] == '127.0.0.1') {
             $mysqli = new mysqli('localhost', 'root', '9#wB$7ppGgjC4g', 'iordinace');
             $mysqli->set_charset('utf8');
-        } //Ebola
-        elseif ($_SERVER['SERVER_ADDR'] == '95.168.206.196') {
+        } elseif ($_SERVER['SERVER_NAME'] == '95.168.206.196') {
+            // Ebola
             $mysqli = new mysqli('mysql2.ebola.cz', 'jwcz_u', 'admin2255', 'jwcz_iordinace');
             $mysqli->set_charset('utf8');
-        } // Ebola
-        elseif ($_SERVER['SERVER_ADDR'] == '95.168.206.203') {
+        } elseif ($_SERVER['SERVER_NAME'] == '95.168.206.203') {
+            // Ebola
             $mysqli = new mysqli('mysql3.ebola.cz', 'iordinacecz_user', '8WK4tXwGP0Cl', 'iordinacecz_sql');
+        } elseif ($_SERVER['SERVER_NAME'] === 'iordinace.jw.cz') {
+            // iordinace.jw.cz
+            $mysqli = new mysqli('127.0.0.1', '8uecqqoi', 'Tt4Xh\vm~r', 'iOrdinace');
+            $mysqli->set_charset('utf8');
         } else {
             $mysqli = new mysqli('localhost', 'root', '9#wB$7ppGgjC4g', 'iordinace');
             $mysqli->set_charset('utf8');
