bbf359d7237b11f94fcdde9e62d1b1526b8cfc52
Jiří Wolf <jiri.wolf@jw.cz>
2021-08-04 10:49:59 +0200

    Připojení k databázi.

--- model/Addaktualita.php
+++ model/Addaktualita.php
@@ -39,7 +39,6 @@ class Addaktualita
     {
         //$mysqli = new mysqli('localhost', 'root', 'heslo', 'iordinace');
         //$mysqli->set_charset('utf8');
-        var_dump($_SERVER['SERVER_NAME']);
         if ($_SERVER['SERVER_NAME'] == '127.0.1.1') {
             $mysqli = new mysqli('localhost', 'root', '9#wB$7ppGgjC4g', 'iordinace');
             $mysqli->set_charset('utf8');
