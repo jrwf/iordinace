55d80e3571bb5a714acb0045e45afc79bc3d74e0
Jiří Wolf <jiri.wolf@jw.cz>
2021-10-22 14:14:20 +0200

    Kontaktní formulář.

--- model/OdesilacEmailu.php
+++ model/OdesilacEmailu.php
@@ -2,23 +2,24 @@
 
 class OdesilacEmailu
 {
-        /**
-         * Odešle email jako HTML, lze tedy používat základní HTML tagy a nové
-         * řádky je třeba psát jako <br /> nebo používat odstavce. Kódování je
-         * odladěno pro UTF-8.
-         *
-         * @param $komu
-         * @param $predmet
-         * @param $zprava
-         * @param $od
-         *
-         * @return bool
-         */
-        public function odesli($komu, $predmet, $zprava, $od)
-        {
-                $hlavicka = "From: " . $od;
-                $hlavicka .= "\nMIME-Version: 1.0\n";
-                $hlavicka .= "Content-Type: text/html; charset=\"utf-8\"\n";
-                return mb_send_mail($komu, $predmet, $zprava, $hlavicka);
-        }
+    /**
+     * Odešle email jako HTML, lze tedy používat základní HTML tagy a nové
+     * řádky je třeba psát jako <br /> nebo používat odstavce. Kódování je
+     * odladěno pro UTF-8.
+     *
+     * @param $komu
+     * @param $predmet
+     * @param $zprava
+     * @param $od
+     *
+     * @return bool
+     */
+    public function odesli($komu, $predmet, $zprava, $od): bool
+    {
+        $hlavicka = "From: " . $od;
+        $hlavicka .= "\nMIME-Version: 1.0\n";
+        $hlavicka .= "Content-Type: text/html; charset=\"utf-8\"\n";
+        return mb_send_mail($komu, $predmet, $zprava);
+//            return mb_send_mail($komu, $predmet, $zprava, $hlavicka);
+    }
 }
