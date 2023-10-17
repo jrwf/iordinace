fac2ef8cab5984bf483ddcd14cebe2bb0d45d877
Jiří Wolf <jiri.wolf@jw.cz>
2023-10-17 07:50:27 +0200

    Přesměrování na administraci.

--- model/Login.php
+++ model/Login.php
@@ -2,39 +2,43 @@
 
 class Login
 {
-  public $hlaska;
-  public $sesna;
+    public $hlaska;
+    public $sesna;
 
-  public function presmerovani() {
-    if($_SERVER['REQUEST_URI'] == '/login') {
-      if(isset($_SESSION['logged_user']) && ($_SESSION['logged_user'] === 'yes')) {
-        header('Location: addaktualita');
-        exit;
-      }
+    /**
+     * @return void
+     */
+    public function presmerovani()
+    {
+        if ($_SERVER['REQUEST_URI'] == '/login') {
+            if (isset($_SESSION['logged_user']) && ($_SESSION['logged_user'] === 'yes')) {
+                header('Location: admin');
+                exit;
+            }
+        }
     }
-  }
 
-  public function logovani()
-  {
-    if($_POST) {
-      if(isset($_POST['username']) && ( $_POST['username'] == 'mprecechtelova@seznam.cz' )) {
-        if( isset($_POST['heslo']) && ( $_POST['heslo'] == 'shotokan' ) ) {
-          $_SESSION['logged_user'] = 'yes';
-          header('Location: addaktualita');
-          exit;
-          //return $this->hlaska;
+    /**
+     * @return string|void
+     */
+    public function logovani()
+    {
+        if ($_POST) {
+            if (isset($_POST['username']) && ($_POST['username'] == 'mprecechtelova@seznam.cz')) {
+                if (isset($_POST['heslo']) && ($_POST['heslo'] == 'shotokan')) {
+                    $_SESSION['logged_user'] = 'yes';
+                    header('Location: admin');
+                    exit;
+                } else {
+                    $this->hlaska = 'Asi preklep, zkuste to jeste jednou.';
+                    return $this->hlaska;
+                }
+            } else {
+                $this->hlaska = 'Zadejte spravne uzivatelske jmeno.';
+                return $this->hlaska;
+            }
         }
-        else {
-          $this->hlaska =  'Asi preklep, zkuste to jeste jednou.';
-          return $this->hlaska;
-        }
-      } 
-      else {
-        $this->hlaska = 'Zadejte spravne uzivatelske jmeno.';
-        return $this->hlaska;
-      }
     }
-  }
 
 }
 
