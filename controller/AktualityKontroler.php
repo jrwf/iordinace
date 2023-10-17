580d79aeb62b2928542dc4cf396783532e29e19e
Jiří Wolf <jiri.wolf@jw.cz>
2023-10-17 16:04:37 +0200

    Zobrazení aktualit na mobilu.

---
+++ controller/AktualityKontroler.php
@@ -0,0 +1,23 @@
+<?php
+
+class AktualityKontroler extends Kontroler
+{
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    public function zpracuj($parametry)
+    {
+        $this->hlavicka = array(
+            'titulek' => 'administrace',
+            'klicova_slova' => '',
+            'popis' => ''
+        );
+
+        $aktuality = new Addaktualita();
+        $this->data['aktuality'] = $aktuality->seznamAktualit();
+
+        $this->pohled = 'aktuality';
+    }
+}
