d779d34dcc7a4c4ceddb3db4410798c764e478ea
Jiří Wolf <jiri.wolf@jw.cz>
2023-10-18 15:59:35 +0200

    Nastavení proměnných.

--- model/DatabaseConnection.php
+++ model/DatabaseConnection.php
@@ -2,12 +2,54 @@
 
 class DatabaseConnection
 {
-    private $host = 'database';
-    private $database = 'iordinace';
-    private $username = 'root';
-    private $password = 'root';
+    protected string $host;
+    protected string $database;
+    protected string $username;
+    protected string $password;
 
-    // TODO - nastavit připojení k databázi pro různe servery
+    public function __construct($host = 'database', $database = 'iordinace', $username = 'root', $password = 'root')
+    {
+        $this->loadEnvironmentVariables(dirname(__DIR__) . '/.env');
+        $environment = getenv('ENVIRONMENT');
+        $config = include dirname(__DIR__) . '/config.php';
+        $envConfig = $config[$environment];
+        $this->host = $envConfig['DB_HOST'];
+        $this->database = $envConfig['DB_DATABASE'];
+        $this->username = $envConfig['DB_USERNAME'];
+        $this->password = $envConfig['DB_PASSWORD'];
+    }
+
+    /**
+     * @param $filePath
+     * @return void
+     */
+    public function loadEnvironmentVariables($filePath): void
+    {
+        if (!file_exists($filePath)) {
+            return;
+        }
+
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        if ($lines === false) {
+            return;
+        }
+
+        foreach ($lines as $line) {
+            // Ignorujte komentáře
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            // Odstraňte uvozovky, pokud existují
+            $value = preg_replace('/^"(.*)"$/', '$1', $value);
+
+            putenv("$name=$value");
+        }
+    }
 
     /**
      * Připojení k databázi
